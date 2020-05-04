9944ebab45e1878a7d9e00d4ddbb857805ce1064
Aniket <arora.aniket@hotmail.com>
2020-05-04 19:37:21 +0200

    Fixed answers in api

--- Project.API/Controllers/QuestionController.cs
+++ Project.API/Controllers/QuestionController.cs
@@ -15,8 +15,36 @@ namespace Project.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class QuestionController : GenericController<Question, QuestionResource, QuestionSaveResource, Guid> {
-        public QuestionController(IQuestionRepo repo, IMapper mapper, ILogger<QuestionController> logger) : base(repo, mapper, logger) {
+        private readonly IQuestionRepo _repo;
+        private readonly IMapper _mapper;
+        private readonly ILogger<QuestionController> _logger;
 
+        public QuestionController(IQuestionRepo repo, IMapper mapper, ILogger<QuestionController> logger) : base(repo, mapper, logger) {
+            this._repo = repo;
+            this._mapper = mapper;
+            this._logger = logger;
+        }
+
+        [HttpPost]
+        public override async Task<IActionResult> Post(QuestionSaveResource saveResource) {
+            try {
+                var entity = _mapper.Map<Question>(saveResource);
+
+                _repo.Create(entity);
+                await _repo.SaveChangesAsync();
+
+                entity = await _repo.GetAsync(entity.Id);
+
+                var resoure = _mapper.Map<QuestionResource>(entity);
+
+                return CreatedAtAction(nameof(GetAll), resoure);
+
+
+            } catch (Exception ex) {
+
+                _logger.LogError(ex.Message);
+                return BadRequest();
+            }
         }
     }
 }
